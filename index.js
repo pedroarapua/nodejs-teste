5282349697705ad62c9dcca2142d1995fd976348
Pedro Henrique F. Dias <pedro.dias@luizalabs.com>
2017-01-13 10:04:54 -0200

    adicionado pm2 para gerenciamento de apps

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const koa = require('koa');
 const app = koa();
 
 app.use(function * (){
-  this.body = 'Hello World 3';
+  this.body = 'Hello World 4';
 });
 
 app.listen(process.env.PORT || 3000);
