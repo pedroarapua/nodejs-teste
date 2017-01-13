12465bdf03045b57488263edaa53e498f7ab2b96
Pedro Henrique F. Dias <pedro.dias@luizalabs.com>
2017-01-13 09:33:01 -0200

    alterado mensagem do hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const koa = require('koa');
 const app = koa();
 
 app.use(function * (){
-  this.body = 'Hello World 1';
+  this.body = 'Hello World 2';
 });
 
 app.listen(process.env.PORT || 3000);
