7ad6e6ecbf9d737ed2e308be2eb91101142f0c20
Pedro Henrique F. Dias <pedroarapua@gmail.com>
2017-01-28 19:30:56 -0200

    teste 12

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const koa = require('koa');
 const app = koa();
 
 app.use(function * (){
-  this.body = 'Hello World 10';
+  this.body = 'Hello World 11';
 });
 
 app.listen(process.env.PORT || 3000);
